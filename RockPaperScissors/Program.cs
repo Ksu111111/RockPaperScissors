947f8ac46ed4fcaec765196d93babffbc2eb700f
Оксана <@DESKTOP-CD3JPIF>
2023-03-23 19:51:47 +0700

    Добавьте файлы проекта.

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // вы проиграли, потому что не можете ввести число от 1 до 3
+        }
+    }
+}
